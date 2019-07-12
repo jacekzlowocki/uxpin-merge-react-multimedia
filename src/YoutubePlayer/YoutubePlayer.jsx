69445d0b04a69234c48c090d7831ffe5d999a79c
jacekzlowocki <jacek.zlowocki@gmail.com>
2019-07-12 14:22:05 +0200

    youtube player props

--- src/YoutubePlayer/YoutubePlayer.jsx
+++ src/YoutubePlayer/YoutubePlayer.jsx
@@ -1,27 +1,96 @@
-import * as Ract from 'react';
+import React from 'react';
 import propTypes from 'proptypes';
 import YouTube from 'react-youtube';
 
+const parseBoolToInt = (bool) => bool ? 1 : 0;
+
 class YouTubePlayer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      player: null,
+    };
+
+    this.onReady = this.onReady.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('nextProps', nextProps);
+  }
+
+  playVideo() {
+    this.state.player.playVideo();
+  }
+
+  pauseVideo() {
+    this.state.player.pauseVideo();
+  }
+
+  onReady(event) {
+    this.setState({ player: event.target });
+  }
+
   render() {
-    const { videoId } = this.props;
+    const {
+      autoplay,
+      controls,
+      height,
+      loop,
+      onEnd,
+      onPause,
+      onPlay,
+      videoId,
+      width,
+    } = this.props;
 
     const opts = {
-      height: '390',
-      width: '640',
+      height,
+      width,
       playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 1
-      }
+        autoplay: parseBoolToInt(autoplay),
+        controls: parseBoolToInt(controls),
+        loop: parseBoolToInt(loop),
+        rel: 0,
+      },
     };
 
     return (
-      <YouTube videoId={videoId} opts={opts} />
+      <YouTube
+        onEnd={onEnd}
+        onPause={onPause}
+        onPlay={onPlay}
+        onReady={this.onReady}
+        opts={opts}
+        videoId={videoId}
+      />
     );
   }
 }
 
 YouTubePlayer.propTypes = {
   videoId: propTypes.string.isRequired,
+  width: propTypes.number,
+  height: propTypes.number,
+  autoplay: propTypes.bool,
+  controls: propTypes.bool,
+  loop: propTypes.bool,
+  onEnd: propTypes.func,
+  onPause: propTypes.func,
+  onPlay: propTypes.func,
+  paused: propTypes.bool,
+};
+
+YouTubePlayer.defaultProps = {
+  autoplay: true,
+  controls: true,
+  height: 360,
+  loop: false,
+  onEnd: () => {},
+  onPause: () => {},
+  onPlay: () => {},
+  paused: false,
+  width: 640,
 };
 
 export default YouTubePlayer;
---
+++ src/YoutubePlayer/presets/0-default.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import YouTubePlayer from '../YoutubePlayer';
+
+export default (
+  <YouTubePlayer autoplay={false} videoId="dQw4w9WgXcQ" uxpId="player1" />
+);
+
